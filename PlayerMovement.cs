b3bd05061747933b4d5991594673cb35c23c3e65
nkuepper <54605237+nkuepper@users.noreply.github.com>
2019-09-10 13:51:54 -0500

    2D Player Movement and Camera Follow
    C# files for Unity.
    Add PlayerMovement to the player object as a component.
    Add followPlayer to the MainCamera object as a component.

---
+++ PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float runSpeed = 8f;
+    public float jumpSpeed = 10f;
+    float hMove = 0f;
+    bool jumping = false;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMove = Input.GetAxis("Horizontal");
+
+        if (rb.velocity.y < 1 && rb.velocity.y >= 0)
+        {
+            if (Input.GetAxis("Jump") > 0 && rb.position.y < 0)
+            {
+                jumping = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (jumping)
+        {
+            rb.velocity = new Vector2(hMove * runSpeed, jumpSpeed);
+            jumping = false;
+        }
+        else
+        {
+            rb.velocity = new Vector2(hMove * runSpeed, rb.velocity.y);
+        }
+        
+        if ((rb.position.x > 8 && rb.velocity.x > 0) || (rb.position.x < -8 && rb.velocity.x < 0))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+}
---
+++ followPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.08f;
+    public Vector3 desiredPosition, position;
+
+    void FixedUpdate()
+    {
+        desiredPosition = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, speed);
+    }
+}
